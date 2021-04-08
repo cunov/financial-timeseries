3ed2936c76db207383a98bc2e7546d3d050fe3bc
Colton Cunov <coltoncunov@gmail.com>
2021-04-08 16:31:58 +0200

    add p1

---
+++ p1.m
@@ -0,0 +1,16 @@
+clear all
+close all
+d = readtable('intel.csv');
+
+X = d.Volume;
+X_miss = d.VolumeMissing;
+Y = log(d.Volume(2:end)) - log(d.Volume(1:end-1));
+Y_miss = log(X_miss(2:end)) - log(X_miss(1:end-1));
+tList = 1:length(Y_miss);
+
+[acf, lags, bounds] = autocorr(Y, 'NumLags', 20);
+plot(lags, acf)
+xlabel('$$h$$', 'Interpreter', 'Latex')
+ylabel('$$\hat{\rho_Y}$$','Interpreter','Latex')
+
+% Choose q = 6 ?
