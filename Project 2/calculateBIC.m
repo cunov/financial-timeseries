bbb16253c8249e4140df3cdbeb6ed130300a100d
Colton Cunov <coltoncunov@gmail.com>
2021-05-17 17:14:08 +0200

    no

--- Project 2/calculateBIC.m
+++
@@ -1,8 +0,0 @@
-function BIC = calculateBIC(n,p,q,var,X)
-    left = (n - (p-q)) .* log( (n.*var) ./ (n-p-q) );
-    middle = n * (1 + log(2*pi));
-    right = (p+q) .* log(sum(X.^2 - n.*var)) ./ (p+q);
-    
-    BIC = left + middle + right;
-end
-
